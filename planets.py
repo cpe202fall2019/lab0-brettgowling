b1b09234616233b0c873ad46d6c8a3f5023b01b7
Brett Gowling <bgowling@calpoly.edu>
2019-09-22 14:44:29 -0700

    Creating main print statement

--- planets.py
+++ planets.py
@@ -1,4 +1,5 @@
 def weight_on_planets():
-   print("What do you weigh on earth?", input(x))
+   weight = input("What do you weigh on earth? ")
+   print("\nOn Mars you would weigh", weight * 0.38, "pounds.\nOn Jupiter you would weigh", weight * 2.34, "pounds.")
 if __name__ == '__main__':
    weight_on_planets()
